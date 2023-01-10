361de35542fc78bedad57140ab791ff022a1ec80
Graham Bygrave <graham@bygrave.org>
2023-01-10 08:49:17 +0000

    Add customer creation form auth restriction using security expression (plus related tests).

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -19,7 +19,7 @@ import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private AbstractRestAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager) {
--- src/test/java/guru/sfg/brewery/web/controllers/CustomerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/CustomerControllerIT.java
@@ -2,12 +2,16 @@ package guru.sfg.brewery.web.controllers;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 @SpringBootTest
 public class CustomerControllerIT extends BaseIT {
@@ -30,4 +34,36 @@ public class CustomerControllerIT extends BaseIT {
         mockMvc.perform(get("/customers"))
                 .andExpect(status().isUnauthorized());
     }
+
+    @DisplayName("Add Customers")
+    @Nested
+    class AddCustomers {
+
+        @Rollback
+        @Test
+        void processCreationForm() throws Exception {
+            mockMvc.perform(post("/customers/new")
+                    .param("customerName", "Foo Customer")
+                    .with(httpBasic("spring", "guru")))
+                    .andExpect(status().is3xxRedirection());
+        }
+
+        @Rollback
+        @ParameterizedTest(name = "#{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.BaseIT#getStreamNotAdmin")
+        void processCreationFormNOTAUTH(String user, String pwd) throws Exception {
+            mockMvc.perform(post("/customers/new")
+                    .param("customerName", "Foo Customer2")
+                    .with(httpBasic(user, pwd)))
+                    .andExpect(status().isForbidden());
+        }
+        
+        @Test
+        void processCreationFormNOAUTH() throws Exception {
+            mockMvc.perform(post("/customers/new")
+                    .param("customerName", "Foo Customer3"))
+                    .andExpect(status().isUnauthorized());
+        }
+    }
+
 }
