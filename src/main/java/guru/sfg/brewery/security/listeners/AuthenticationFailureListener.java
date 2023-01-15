f2c8e53661c038b4c98936a4f4589a120ee1cf1d
Graham Bygrave <graham@bygrave.org>
2023-01-15 17:45:29 +0000

    Explicitly check if the username is legitimate when a failure occurs. If it is, record associate the user record with the failure. If it isn't, just record the bogus username.

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
@@ -1,5 +1,7 @@
 package guru.sfg.brewery.security.listeners;
 
+import java.util.Optional;
+
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Component;
 import guru.sfg.brewery.domain.security.LoginFailure;
 import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.LoginFailureRepository;
+import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,31 +19,29 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class AuthenticationFailureListener {
-    
+
     private final LoginFailureRepository loginFailureRepository;
-    
+    private final UserRepository         userRepository;
+
     @EventListener
     public void listen(AuthenticationFailureBadCredentialsEvent event) {
         log.debug("Login attempt with bad credentials");
-        
+
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
-            
+
             LoginFailure.LoginFailureBuilder builder = LoginFailure.builder();
-            
-            UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken)event.getSource();
-            if (token.getPrincipal() instanceof User) {
-                User user = (User) token.getPrincipal();
-                builder.user(user);
-                builder.username(user.getUsername());
-            } else if (token.getPrincipal() instanceof String) {
+
+            UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
+            if (token.getPrincipal() instanceof String) {
                 String username = (String) token.getPrincipal();
                 log.debug("Attempted login with username: " + username);
                 builder.username(username);
+                userRepository.findByUsername(username).ifPresent(builder::user);;
             }
-            
+
             if (token.getDetails() instanceof WebAuthenticationDetails) {
-                WebAuthenticationDetails details = (WebAuthenticationDetails)token.getDetails();
-                
+                WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
+
                 builder.sourceIp(details.getRemoteAddress());
                 log.debug("Source IP: " + details.getRemoteAddress());
             }
