1cad80bc52a2bc9e36080799fb9352c9c5308587
Graham Bygrave <graham@bygrave.org>
2023-01-16 08:48:23 +0000

    Periodically unlock locked user accounts.

--- src/main/java/guru/sfg/brewery/repositories/security/UserRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/UserRepository.java
@@ -1,9 +1,13 @@
 package guru.sfg.brewery.repositories.security;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import guru.sfg.brewery.domain.security.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+    
+    List<User> findAllByAccountNonLockedAndLastModifiedDateIsBefore(Boolean accountNonLocked, Timestamp timestamp);
 }
