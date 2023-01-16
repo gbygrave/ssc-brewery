6fc4c0e5b4b1f144e0356c90a8bb4b403bbd2efa
Graham Bygrave <graham@bygrave.org>
2023-01-16 09:40:18 +0000

    Add GoogleAuthenticator bean and generate QR code in controller.
    Add/configure Google authenticator bean and tie into credential
    repository.
    Update UserController to call out to authenticator and generate the TOTP
    dynamic QR code url.

--- src/main/java/guru/sfg/brewery/config/SecurityBeans.java
+++ src/main/java/guru/sfg/brewery/config/SecurityBeans.java
@@ -1,5 +1,7 @@
 package guru.sfg.brewery.config;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationEventPublisher;
@@ -10,14 +12,31 @@ import org.springframework.security.authentication.DefaultAuthenticationEventPub
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorConfig;
+import com.warrenstrange.googleauth.ICredentialRepository;
+
 @Configuration
 public class SecurityBeans {
-    
+
+    @Bean
+    GoogleAuthenticator googleAuthenticator(ICredentialRepository credentialRepository) {
+        GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder configBuilder = new GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder();
+
+        configBuilder.setTimeStepSizeInMillis(TimeUnit.SECONDS.toMillis(60))
+                .setWindowSize(10)
+                .setNumberOfScratchCodes(0);
+
+        GoogleAuthenticator googleAuthenticator = new GoogleAuthenticator(configBuilder.build());
+        googleAuthenticator.setCredentialRepository(credentialRepository);
+        return googleAuthenticator;
+    }
+
     @Bean
     AuthenticationEventPublisher authenticationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
         return new DefaultAuthenticationEventPublisher();
     }
-    
+
     @Bean
     PersistentTokenRepository persistentTokenRepository(DataSource dataSource) {
         JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -1,5 +1,6 @@
 package guru.sfg.brewery.web.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+
+import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,14 +22,29 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserRepository userRepository;
-    
+    private final UserRepository      userRepository;
+    private final GoogleAuthenticator googleAuthenticator;
+
     @GetMapping("/register2fa")
     public String register2fa(Model model) {
-        model.addAttribute("googleurl", "todo");
+
+        User user = getUser();
+
+        String url = GoogleAuthenticatorQRGenerator.getOtpAuthURL(
+                "SFG",
+                user.getUsername(),
+                googleAuthenticator.createCredentials(user.getUsername()));
+
+        log.debug("Google QR URL: " + url);
+        
+        model.addAttribute("googleurl", url);
         return "user/register2fa";
     }
-    
+
+    private User getUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
     @PostMapping("/register2fa")
     public String confirm2Fa(@RequestParam Integer verifyCode) {
         // TODO: implement
